6618f93797525cfb7565326fcd9ae6a932e028ae
John Gaitho <johngaitho05@gmail.com>
2023-03-16 06:16:30 -0700

    update script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: Prints exactly "Programming is like
+ * building a multilingual puzzle,followed by a new line.
+ * Return: 0
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle");
 return (0);
 }
-
