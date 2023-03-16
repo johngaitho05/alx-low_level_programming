5c7b33de60d20f8bbc62b6e48808a413aca6e9a4
John Gaitho <johngaitho05@gmail.com>
2023-03-16 06:43:09 -0700

    update script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,12 @@
  */
 int main(void)
 {
-printf("Size of a char %lo byte(s)\n", sizeof(char));
-printf("Size of a int %lo byte(s)\n", sizeof(int));
-printf("Size of a long int %lo byte(s)\n", sizeof(long int));
-printf("Size of a long long %lo byte(s)\n", sizeof(long long int));
-printf("Size of a float %lo byte(s)\n", sizeof(float));
+printf("Size of a char: %lo byte(s)\n", sizeof(char));
+printf("Size of an int: %lo byte(s)\n", sizeof(int));
+printf("Size of a long int: %lo byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lo byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lo byte(s)\n", sizeof(float));
 return (0);
 }
+
 
