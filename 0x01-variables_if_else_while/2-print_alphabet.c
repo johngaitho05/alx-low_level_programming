3423267b36749fa718e89e30fdaa514ce1da1728
John Gaitho <johngaitho05@gmail.com>
2023-03-17 07:37:48 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Generates a random number and
+ * specifies whether the generated
+ * number is greater than 5, less than 6 or 0
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+{
+putchar(c);
+putchar('\n');
+}
+return (0);
+}
