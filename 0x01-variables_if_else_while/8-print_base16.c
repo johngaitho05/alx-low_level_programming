eaaa7dd2334524c0d6acf0ba8f1febe991d93fb9
John Gaitho <johngaitho05@gmail.com>
2023-03-17 08:09:36 -0700

    print base 16 numbers in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Generates a random number and
+ * specifies whether the generated
+ * number is greater than 5, less than 6 or 0
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(48 + i);
+}
+
+for (c = 'a'; c <= 'f'; ++c)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
+
