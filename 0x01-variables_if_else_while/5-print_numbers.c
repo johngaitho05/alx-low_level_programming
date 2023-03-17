857c42ca6c70f238523c99a8d1b85247fc86732c
John Gaitho <johngaitho05@gmail.com>
2023-03-17 07:54:39 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Generates a random number and
+ * specifies whether the generated
+ * number is greater than 5, less than 6 or 0
+ * Return: 0
+ */
+int main(void)
+{
+char i;
+for (i = 0; i < 10; ++i)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
+
