29c20452901645fc23cb61345c4a58092248ee27
John Gaitho <johngaitho05@gmail.com>
2023-03-17 08:31:08 -0700

    print one digit combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Generates a random number and
+ * specifies whether the generated
+ * number is greater than 5, less than 6 or 0
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+int i;
+int j;
+for (i = 0; i < 10; i++)
+{
+j = 0;
+while (j < 10)
+{
+putchar(48 + i);
+putchar(48 + j);
+putchar(',');
+putchar(' ');
+++j;
+}
+}
+putchar('\n');
+return (0);
+}
+
