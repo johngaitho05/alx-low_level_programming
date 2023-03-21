8d2e5b39329935e35a6f676ac5082219572b0f76
John Gaitho <johngaitho05@gmail.com>
2023-03-21 11:34:30 -0700

    fix mixed declaration

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -52,18 +52,12 @@ return (1);
 
 void print_times_table(int n)
 {
+int i, j, k, l, m, s, spaces;
+int *numbers;
 if (n < 0 || n > 15)
 {
 return;
 }
-int i;
-int j;
-int k;
-int l;
-int m;
-int s;
-int spaces;
-int *numbers;
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
