af6d386195efa5a81b83f1a2fd3f0b7046778ff9
John Gaitho <johngaitho05@gmail.com>
2023-03-23 18:05:46 +0300

    update logic

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -4,28 +4,20 @@
  * main - prints largest prime factor.
  * Return: Always 0.
  */
+
 int main(void)
 {
-long int n;
-n = 612852475143;
-long int div = 2, ans = 0, maxFact;
-while (n != 0)
-{
-if (n % div != 0)
-{
-div = div + 1;
-}
-else
-{
-maxFact = n;
-n = n / div;
-if (n == 1)
-{
-printf("%d\n", maxFact);
-ans = 1;
-break;
-}
-}
-}
-return (0);
+	long int n, fp;
+
+	n = 612852475143;
+	for (fp = 2; fp <= n; fp++)
+	{
+		if (n % fp == 0)
+		{
+			n /= fp;
+			fp--;
+		}
+	}
+	printf("%ld\n", fp);
+	return (0);
 }
