3cb5e271710729616bc23bfd536f72f738f5d586
John Gaitho <johngaitho05@gmail.com>
2023-03-23 18:04:31 +0300

    find largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints largest prime factor.
+ * Return: Always 0.
+ */
+int main(void)
+{
+long int n;
+n = 612852475143;
+long int div = 2, ans = 0, maxFact;
+while (n != 0)
+{
+if (n % div != 0)
+{
+div = div + 1;
+}
+else
+{
+maxFact = n;
+n = n / div;
+if (n == 1)
+{
+printf("%d\n", maxFact);
+ans = 1;
+break;
+}
+}
+}
+return (0);
+}
