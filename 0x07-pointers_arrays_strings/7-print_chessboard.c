b38a7f627d9120c90ac44e67262a3252f7b2f5e1
John Gaitho <johngaitho05@gmail.com>
2023-04-03 23:29:22 +0300

    print chess board

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard.
+ * @board: pointer to the 2D array representing the chessboard.
+ *
+ * Return: void.
+ */
+void print_chessboard(char (*board)[8])
+{
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(board[row][col]);
+		}
+		_putchar('\n');
+	}
+}
