ce60cc19728b601fbb2284fb46f21197bf8e32f2
John Gaitho <johngaitho05@gmail.com>
2023-04-03 21:42:16 +0300

    apply betty style

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte.
- * @s: pointer to the memory area to fill
- * @b: the constant byte to fill the memory with
- * @n: the number of bytes to fill
+ * _memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
  *
  * Return: pointer to the memory area s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	char *p = s;
-	while (n-- > 0) {
-		*p++ = b;
+	unsigned int j;
+
+	for (j = 0; j < n; j++)
+	{
+		s[j] = b;
 	}
 	return (s);
 }
