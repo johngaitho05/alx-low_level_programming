0b5cc40ee4015b96e0cfdbb37b1da7c6a5680edd
John Gaitho <johngaitho05@gmail.com>
2023-04-03 21:31:56 +0300

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte.
+ * @s: pointer to the memory area to fill
+ * @b: the constant byte to fill the memory with
+ * @n: the number of bytes to fill
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n) {
+	char *p = s;
+	while (n-- > 0) {
+		*p++ = b;
+	}
+	return s;
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef ALX_LOW_LEVEL_PROGRAMMING_MAIN_H
+#define ALX_LOW_LEVEL_PROGRAMMING_MAIN_H
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif //ALX_LOW_LEVEL_PROGRAMMING_MAIN_H
