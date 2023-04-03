deb1966d68aa17f31fe5d676ec31f6818798a365
John Gaitho <johngaitho05@gmail.com>
2023-04-03 22:46:23 +0300

    more refactor

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * _strpbrk - searches a string for any of a set of bytes
@@ -23,5 +22,5 @@ char *_strpbrk(char *s, char *accept)
 		}
 	}
 
-	return (NULL);
+	return ('\0');
 }
