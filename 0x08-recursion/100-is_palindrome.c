922ee38028ec182c9a392961cb74a77c7083694f
John Gaitho <johngaitho05@gmail.com>
2023-04-05 22:29:08 +0300

    is palidndrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,35 @@
+#include "string.h"
+/**
+ * _is_palindrome - helper function to check if string is palindrome
+ * @s: the string to check
+ * @i: first index yo compare
+ * @j: second index to compare
+ * Return: 1 if palindrome else 0
+ */
+int _is_palindrome(char *s, int i, int j)
+{
+	if (s[i] != s[j])
+	{
+		return (0);
+	}
+	if (i >= j)
+	{
+		return (1);
+	}
+	return (_is_palindrome(s, i + 1, j - 1));
+}
+
+/**
+ * is_palindrome - checks whether a string is palindrome
+ * @s: the string to check
+ * Return: 1 if palindrome else 0
+ */
+int is_palindrome(char *s)
+{
+	size_t length = strlen(s);
+
+	if (length == 0)
+		return (0);
+
+	return (_is_palindrome(s, 0, length - 1));
+}
