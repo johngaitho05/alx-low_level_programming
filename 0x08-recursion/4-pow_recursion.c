1b3b0d1c6d385ed836d2639a8a9591b67aa82323
John Gaitho <johngaitho05@gmail.com>
2023-04-05 17:38:56 +0300

    x to power y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+/**
+ * _pow_recursion - computes the value of x^y
+ * @x: the base number
+ * @y: the power number
+ * Return: x^y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y == 1)
+	{
+		return (x);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
