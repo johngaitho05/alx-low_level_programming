6d19f6c1af186086710910d6f43f1f7393c215f4
John Gaitho <johngaitho05@gmail.com>
2023-04-09 16:09:28 +0300

    reformat

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,12 @@
 
 /**
  * main - prints the name of the program
- * @argc  - number of command line arguments
- * @argv - pointer to the command line arguments
+ * @argc: number of command line arguments
+ * @argv: pointer to the command line arguments
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
