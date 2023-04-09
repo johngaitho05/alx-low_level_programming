298e73010d3f05ff0741de9f23bbf082aaba3c72
John Gaitho <johngaitho05@gmail.com>
2023-04-09 16:20:29 +0300

    print args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+/**
+ * main - prints all passed arguments
+ * @argc: number of command line arguments
+ * @argv: pointer to the command line arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 1; i < argc; i++)
+	printf("%s\n", argv[i]);
+
+	return (0);
+}
