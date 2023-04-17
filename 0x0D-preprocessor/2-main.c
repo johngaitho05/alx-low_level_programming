f41492d9ef9d43d31a28d4430ce0543b9358d947
John Gaitho <johngaitho05@gmail.com>
2023-04-17 18:45:40 +0300

    Filename

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define FILENAME __FILE__
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", FILENAME);
+	return (0);
+}
