e2e528d3b6e05c7ea772c3df3341402de2b1ca4e
John Gaitho <johngaitho05@gmail.com>
2023-04-28 05:12:15 +0300

    refactor

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,19 +2,17 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
- *
+ * list_len - counts the number of elements in a linked list
+ * @h: pointer to the list
  * Return: number of elements in h
  */
 size_t list_len(const list_t *h)
 {
-	size_t n = 0;
+	size_t length = 0;
 
-	while (h)
-	{
-		n++;
-		h = h->next;
+	for (const list_t *node = h; node != NULL; node = node->next) {
+		length++;
 	}
-	return (n);
+
+	return (length);
 }
