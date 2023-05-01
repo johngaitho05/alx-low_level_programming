8f323106a43ffd9462c42d4ba158af2ea9be51ad
John Gaitho <johngaitho05@gmail.com>
2023-05-02 00:45:24 +0300

    refactor

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,24 +1,25 @@
 #include "stdlib.h"
 #include "lists.h"
+
 /**
-* free_listint2 - free linked list and set head to null
- * @head: the linked list to be fred
-*/
+ * free_listint2 - frees a linked list by setting head to null
+ * @head: the head node
+ */
+
 void free_listint2(listint_t **head)
 {
 	listint_t *nxt;
 
-	if (*head == NULL)
-	{
+	if (head == NULL)
 		return;
-	}
 
 	while (*head)
 	{
 		nxt = (*head)->next;
 		free(*head);
 		*head = nxt;
-
 	}
+
 	*head = NULL;
 }
+
