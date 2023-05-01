35d9cfb24bb600cc752be54e30e811f7ab95e570
John Gaitho <johngaitho05@gmail.com>
2023-05-01 23:22:28 +0300

    betty style

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,4 +1,5 @@
 #include "stdlib.h"
+#include "stdio.h"
 #include "lists.h"
 
 /**
@@ -11,10 +12,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
-	new = malloc(sizeof (listint_t));
+	new = malloc(sizeof(listint_t));
 
 	if (new == NULL)
+	{
 		return (NULL);
+	}
 
 	new->n = n;
 	new->next = *head;
