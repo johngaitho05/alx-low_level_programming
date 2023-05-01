f35a215a5df5037f9106c94dad1f7e6bbc43320f
John Gaitho <johngaitho05@gmail.com>
2023-05-01 22:39:51 +0300

    count nodes

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+#include "lists.h"
+
+
+/**
+ * listint_len - counts the number of nodes in a linked list
+ * @h: the linked list
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num_nodes = 0;
+
+	while (h)
+	{
+		num_nodes++;
+		h = h->next;
+	}
+
+	return (num_nodes);
+
+}
