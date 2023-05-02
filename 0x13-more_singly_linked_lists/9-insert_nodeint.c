02ed06646f2d5dc545fe1098894ff93e24ccb3c6
John Gaitho <johngaitho05@gmail.com>
2023-05-02 14:43:55 +0300

    polish

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,10 +39,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		current = current->next;
 	}
-	if(idx == count && current == NULL)
-	{
-
-	}
 
 	return (NULL);
 }
