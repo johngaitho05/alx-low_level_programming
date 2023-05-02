52814a634a7e2b7e6ef3306d80dff7ba81d16d53
John Gaitho <johngaitho05@gmail.com>
2023-05-02 13:47:42 +0300

    refactor

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,29 +2,27 @@
 #include "lists.h"
 
 /**
- * *insert_nodeint_at_index - insert a node at index;
- * @head: the header node
- * @idx: position to insert the new node
- * @n: value of the new node
- * Return: address to the new node
+ * insert_nodeint_at_index - inserts new node at a given position
+ * @head: the head node
+ * @idx: the position to insert the new node
+ * @n: the value to insert
+ *
+ * Return: pointer to the new node or NULL
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx,
+                                   int n)
 {
+	unsigned int i;
 	listint_t *new;
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	unsigned int count = 0;
+	listint_t *temp = *head;
 
 	new = malloc(sizeof(listint_t));
-
-	if (new == NULL)
+	if (new == NULL || *head == NULL)
 		return (NULL);
 
 	new->n = n;
 	new->next = NULL;
-
-	if (*head == NULL)
-		return (new);
 
 	if (idx == 0)
 	{
@@ -33,18 +31,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	while (current && count < idx)
+	for (i = 0; temp && i < idx; i++)
 	{
-		prev = current;
-		current = current->next;
-		count++;
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
 	}
-	if (!current)
-		return (NULL);
 
-	if (prev)
-		prev->next = new;
-	new->next = current;
-
-	return (new);
+	return (NULL);
 }
+
