516d36015f3634870e13b68706b0355170e9c877
John Gaitho <johngaitho05@gmail.com>
2023-05-02 15:13:10 +0300

    delete node at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,18 +2,37 @@
 #include "lists.h"
 
 /**
- *
  * delete_nodeint_at_index - delete element at given index
+ * @head: pointer to the first node
  * @index: the index of the deletion candidate
  * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *tmp = *head;
+	unsigned int count = 0;
+	listint_t *prev = NULL;
+	listint_t  *current = *head;
 
-	if(*head == NULL){
+	if (*head == NULL)
 		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		return (1);
 	}
-	while (index)
+	while (current && count < index)
+	{
+		prev = current;
+		current = current->next;
+		count++;
+	}
+	if (current)
+	{
+		prev->next = current->next;
+		free(current);
+		return (1);
+	}
+	return (-1);
 
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -40,7 +40,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 	}
 
-	free(current);
-
 	return (NULL);
 }
