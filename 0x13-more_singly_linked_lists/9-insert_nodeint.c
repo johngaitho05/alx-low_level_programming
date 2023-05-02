79819ecdd295924047300544fd7d38b4ae1ec669
John Gaitho <johngaitho05@gmail.com>
2023-05-02 13:25:46 +0300

    handle id=1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,6 +26,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (*head == NULL)
 		return (new);
 
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 
 	while (current && count < idx)
 	{
