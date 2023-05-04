96b67a55537262a9ec3ebc4bfa0767467f0910b0
John Gaitho <johngaitho05@gmail.com>
2023-05-04 23:01:57 +0300

    decimal to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+
+/**
+ * print_binary - convert decimal ti binary
+ * @n: the decimal number to convert
+ */
+void print_binary(unsigned long int n)
+{
+	int i;
+	unsigned long int increment;
+	unsigned long int closest = 1;
+	int pow = 0;
+	unsigned long int next = 2;
+
+	if (n == 0)
+	{
+		_putchar(48);
+		return;
+	}
+
+	while (next <= n)
+	{
+		closest = 1 << (pow + 1);
+		next = closest + (1 << (pow + 1));
+		pow++;
+	}
+
+	_putchar(49);
+	for (i = pow - 1; i >= 0; i--)
+	{
+		increment = 1 << i;
+		if (closest + increment <= n)
+		{
+			closest += increment;
+			_putchar(49);
+		}
+		else
+		{
+			_putchar(48);
+		}
+	}
+}
