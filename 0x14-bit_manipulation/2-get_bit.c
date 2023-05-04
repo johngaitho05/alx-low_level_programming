84b61ad9f0225fd3bb2b7f7d6ed03304b97b34f4
John Gaitho <johngaitho05@gmail.com>
2023-05-05 00:31:35 +0300

    print binary

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,20 +1,26 @@
 #include "main.h"
 
 /**
- * get_bit - get bit at given index
- * @n: decimal number
- * @index: index of the bit to be located
- *
- * Return: value of the bit
+ * print_binary - prints the binary equivalent of a given number
+ * @n: decimal number fro which to print binary equivalent
  */
-int get_bit(unsigned long int n, unsigned int index)
+void print_binary(unsigned long int n)
 {
-	int x;
+	int i, j = 0;
+	unsigned long int nxt;
 
-	if (index > 63)
-		return (-1);
+	for (i = 63; i >= 0; i--)
+	{
+		nxt = n >> i;
 
-	x = (n >> index) & 1;
-
-	return (x);
+		if (nxt & 1)
+		{
+			_putchar('1');
+			j++;
+		}
+		else if (j)
+			_putchar('0');
+	}
+	if (!j)
+		_putchar('0');
 }
