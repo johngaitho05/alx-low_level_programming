b1b8ef9fbc43e33f373aaf116c60d3ca39d6dc00
John Gaitho <johngaitho05@gmail.com>
2023-05-08 18:30:45 +0300

    betty style

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,24 +3,35 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+
+/**
+ * read_textfile - print the first n letters chars of a file
+ * @filename: The file to be printed
+ * @letters: Number of letters to print
+ * Return: Number of printed letters
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, rb, wb;
 	char *buff_size = malloc(letters * sizeof(char));
 
-	if(filename == NULL) return (0);
+	if (filename == NULL)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 
-	if(fd == -1) return (0);
+	if (fd == -1)
+		return (0);
 
-	rb = read(fd, buff_size,letters);
+	rb = read(fd,  buff_size, letters);
 
-	if(rb == -1) return (0);
+	if (rb == -1)
+		return (0);
 
 	wb = write(STDOUT_FILENO, buff_size, letters);
 
-	if(wb == -1) return (0);
+	if (wb == -1)
+		return (0);
 
 	return (rb);
 }
