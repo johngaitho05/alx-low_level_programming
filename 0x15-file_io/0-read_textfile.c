65812e736e15f81044b131f74e9b8d0664bf5b9a
John Gaitho <johngaitho05@gmail.com>
2023-05-08 18:49:23 +0300

    refactor

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,18 +13,19 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, rb, wb;
-	char *buff_size = malloc(letters * sizeof(char));
+	char *buff_size;
 
-	if (!filename || !buff_size)
+	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
 
 	if (fd < 0)
-	{
-		free(buff_size);
 		return (0);
-	}
+
+	buff_size = malloc(letters * sizeof(char));
+	if(!buff_size)
+		return (0);
 
 	rb = read(fd,  buff_size, letters);
 
