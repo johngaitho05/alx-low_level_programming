606255a9bf5f98ec4adcf74fa8c0b987d286e5f1
John Gaitho <johngaitho05@gmail.com>
2023-05-08 19:08:39 +0300

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - create a file and paste content
+ * @filename: the name of the file
+ * @text_content: content to paste to the file
+ *
+ * Return: 1 if success else -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file, rs, ws = 0;
+
+	if (!filename)
+		return (-1);
+
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (file < 0)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[ws])
+			ws++;
+		rs = write(file, text_content, ws);
+		if (rs != ws)
+			return (-1);
+	}
+
+	close(file);
+
+	return (1);
+}
