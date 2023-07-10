97438f82197640be91ffccb7e1696dc3c3e240f4
John Gaitho <johngaitho05@gmail.com>
2023-07-10 16:22:05 +0300

    polish

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,35 +2,29 @@
 
 /**
  * read_textfile - reads a text file and prints to the POSIX standard output
- * @filename: the name of the file to read
- * @letters: the number of letters to read and print
+ * @filename: file to read
+ * @letters: number of letters to read
  *
- * Return: actual number of letters read and printed, or 0 if an error occurs
+ * Return: number of letters read, 0 if failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int rder;
-	ssize_t txt, rxt;
-	char *buf;
+	int count;
+	ssize_t rp, wp;
+	char *buffer;
 
 	if (!filename)
 		return (0);
-
-	rder = open(filename, O_RDONLY);
-
-	if (rder == -1)
+	count = open(filename, O_RDONLY);
+	if (count == -1)
 		return (0);
-
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
 		return (0);
+	rp = read(count, buffer, letters);
+	wp = write(STDOUT_FILENO, buffer, rp);
+	close(count);
+	free(buffer);
 
-	txt = read(rder, buf, letters);
-	rxt = write(STDOUT_FILENO, buf, txt);
-
-	close(rder);
-
-	free(buf);
-
-	return (rxt);
+	return (wp);
 }
