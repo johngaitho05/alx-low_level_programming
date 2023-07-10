6837e4967499d991a2b9da943b1c8875a2609e36
John Gaitho <johngaitho05@gmail.com>
2023-07-10 15:52:47 +0300

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,18 +14,32 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
-		return (0);
+		return (ssize_t)(0);
+
 	buff_size = malloc(letters * sizeof(char));
 	if (!buff_size)
-		return (0);
+		return (ssize_t)(0);
+
 	rb = read(fd, buff_size, letters);
 	if (rb < 0)
-		return (0);
+	{
+		free(buff_size);
+		return (ssize_t)(0);
+	}
 	buff_size[rb] = '\0';
+
 	close(fd);
+
 	wb = write(STDOUT_FILENO, buff_size, rb);
+
+	if (wb < 0)
+	{
+		free(buff_size);
+		return (ssize_t)(0);
+	}
 	free(buff_size);
 	return (wb);
 }
