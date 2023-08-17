7c5e3f9623d74915b16eadfede691111b2cb06b6
John Gaitho <johngaitho05@gmail.com>
2023-08-17 23:06:01 +0300

    keygen

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -11,23 +11,23 @@
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	char secrect[7], *hash;
+	char password[7], *codex;
 	int len = strlen(argv[1]), i, tmp;
 
-	hash = "H-AHvDw87lNS0E9j2TibgpnMVys5XzvtOGJcYLU+4mjL6fxqZeF3Qa1rKhdKIouk";
+	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
 	tmp = (len ^ 59) & 63;
-	secrect[0] = hash[tmp];
+	password[0] = codex[tmp];
 
 	tmp = 0;
 	for (i = 0; i < len; i++)
 		tmp += argv[1][i];
-	secrect[1] = hash[(tmp ^ 79) & 63];
+	password[1] = codex[(tmp ^ 79) & 63];
 
 	tmp = 1;
 	for (i = 0; i < len; i++)
 		tmp *= argv[1][i];
-	secrect[2] = hash[(tmp ^ 85) & 63];
+	password[2] = codex[(tmp ^ 85) & 63];
 
 	tmp = 0;
 	for (i = 0; i < len; i++)
@@ -36,18 +36,18 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 			tmp = argv[1][i];
 	}
 	srand(tmp ^ 14);
-	secrect[3] = hash[rand() & 63];
+	password[3] = codex[rand() & 63];
 
 	tmp = 0;
 	for (i = 0; i < len; i++)
 		tmp += (argv[1][i] * argv[1][i]);
-	secrect[4] = hash[(tmp ^ 239) & 63];
+	password[4] = codex[(tmp ^ 239) & 63];
 
 	for (i = 0; i < argv[1][0]; i++)
 		tmp = rand();
-	secrect[5] = hash[(tmp ^ 229) & 63];
+	password[5] = codex[(tmp ^ 229) & 63];
 
-	secrect[6] = '\0';
-	printf("%s", secrect);
+	password[6] = '\0';
+	printf("%s", password);
 	return (0);
 }
