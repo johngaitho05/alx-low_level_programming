8dadd4134edea2e1b6ef626ac8408d88883047bc
John Gaitho <johngaitho05@gmail.com>
2023-08-17 23:15:55 +0300

    keygen

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 /**
- * main - Generates and prints passwords for the crackme5 executable.
+ * main - Generates and prints keys for the crackme5 executable.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
@@ -11,43 +11,43 @@
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	char password[7], *codex;
-	int len = strlen(argv[1]), i, tmp;
+	char key[7], *codex;
+	int count = strlen(argv[1]), i, cache;
 
 	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
-	tmp = (len ^ 59) & 63;
-	password[0] = codex[tmp];
+	cache = (count ^ 59) & 63;
+	key[0] = codex[cache];
 
-	tmp = 0;
-	for (i = 0; i < len; i++)
-		tmp += argv[1][i];
-	password[1] = codex[(tmp ^ 79) & 63];
+	cache = 0;
+	for (i = 0; i < count; i++)
+		cache += argv[1][i];
+	key[1] = codex[(cache ^ 79) & 63];
 
-	tmp = 1;
-	for (i = 0; i < len; i++)
-		tmp *= argv[1][i];
-	password[2] = codex[(tmp ^ 85) & 63];
+	cache = 1;
+	for (i = 0; i < count; i++)
+		cache *= argv[1][i];
+	key[2] = codex[(cache ^ 85) & 63];
 
-	tmp = 0;
-	for (i = 0; i < len; i++)
+	cache = 0;
+	for (i = 0; i < count; i++)
 	{
-		if (argv[1][i] > tmp)
-			tmp = argv[1][i];
+		if (argv[1][i] > cache)
+			cache = argv[1][i];
 	}
-	srand(tmp ^ 14);
-	password[3] = codex[rand() & 63];
+	srand(cache ^ 14);
+	key[3] = codex[rand() & 63];
 
-	tmp = 0;
-	for (i = 0; i < len; i++)
-		tmp += (argv[1][i] * argv[1][i]);
-	password[4] = codex[(tmp ^ 239) & 63];
+	cache = 0;
+	for (i = 0; i < count; i++)
+		cache += (argv[1][i] * argv[1][i]);
+	key[4] = codex[(cache ^ 239) & 63];
 
 	for (i = 0; i < argv[1][0]; i++)
-		tmp = rand();
-	password[5] = codex[(tmp ^ 229) & 63];
+		cache = rand();
+	key[5] = codex[(cache ^ 229) & 63];
 
-	password[6] = '\0';
-	printf("%s", password);
+	key[6] = '\0';
+	printf("%s", key);
 	return (0);
 }
