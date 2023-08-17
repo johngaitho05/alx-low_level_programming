1570575fdef3ad531c7c971482689ab026ba8c8e
John Gaitho <johngaitho05@gmail.com>
2023-08-17 20:54:51 +0300

    insert at the end

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - inserts a node at the end of a dll
+ * @head: pointer to the header node
+ * @n: value of the new node
+ * Return: the newly created node if successful else NULL
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *current = *head;
+
+	if (new == NULL)
+		return (NULL); /* memory allocation failed */
+	new->n = n;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	while (current->next != NULL)
+		current = current->next;
+
+	current->next = new;
+	return (new);
+}
