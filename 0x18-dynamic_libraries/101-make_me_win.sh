77bb84daea4dc46c1ee3a2365c9bc574e08ba9ee
John Gaitho <johngaitho05@gmail.com>
2023-08-21 21:45:25 +0300

    winhack

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/johngaitho05/alx-low_level_programming/tree/master/0x18-dynamic_libraries/winhack.so
-export LD_PRELOAD=/tmp/winhack.so
+export LD_PRELOAD=./winhack.so
