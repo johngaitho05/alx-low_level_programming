c87f29694992813328af0fc6b70c62bd53870e8d
John Gaitho <johngaitho05@gmail.com>
2023-08-21 22:06:15 +0300

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/samuelselasi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/gm_crack.so
-export LD_PRELOAD=/tmp/gm_crack.so
+wget -P /tmp https://github.com/johngaitho05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/jackpot.so
+export LD_PRELOAD=/tmp/jackpot.so
