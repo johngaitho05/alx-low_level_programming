096b03376cea85f19239ee42915863393733c55b
John Gaitho <johngaitho05@gmail.com>
2023-08-21 16:39:14 +0300

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://raw.github.com/johngaitho05/alx-low_level_programming/tree/master/0x18-dynamic_libraries/winner.so
+wget -P /tmp https://github.com/johngaitho05/alx-low_level_programming/tree/master/0x18-dynamic_libraries/winner.so
 export LD_PRELOAD=/tmp/winner.so
