8d93d7d88a98977f45991e771206bac5d8e93114
John Gaitho <johngaitho05@gmail.com>
2023-09-08 21:18:19 +0300

    insert: refactor

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,53 +1,66 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Adds or updates a key-value pair in the hash table.
- * @ht: The hash table to add/update the key-value pair.
- * @key: The key to add/update.
- * @value: The value associated with the key.
+ * add_n_hash - adds a node at the beginning of a hash at a given index
+ * @head: head of the hash linked list
+ * @key: key of the hash
+ * @value: value to store
  *
- * Return: 1 if it succeeded, 0 otherwise.
+ * Return: head of the hash
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+
+hash_node_t *add_n_hash(hash_node_t **head, const char *key, const char *value)
 {
-
-	unsigned long int index;
-	hash_node_t *new_node = NULL;
 	hash_node_t *current;
-	char *new_value;
 
-	index = key_index((const unsigned char *)key, ht->size);
-	current = ht->array[index];
-	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
-		return (0);
+	current = *head;
+
 	while (current != NULL)
 	{
-		if (strcmp(current->key, key) == 0)
+		if (strcmp(key, current->key) == 0)
 		{
-			/* Update the value associated with the existing key */
-			new_value = strdup(value);
-			if (new_value == NULL)
-				return (0);
 			free(current->value);
-			current->value = new_value;
-			return (1);
+			current->value = strdup(value);
+			return (*head);
 		}
 		current = current->next;
 	}
-	 /* If the key doesn't exist, create a new node and add it to the list*/
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+
+	current = malloc(sizeof(hash_node_t));
+
+	if (current == NULL)
+		return (NULL);
+
+	current->key = strdup(key);
+	current->value = strdup(value);
+	current->next = *head;
+	*head = current;
+
+	return (*head);
+}
+
+/**
+ * hash_table_set - adds a hash (key, value) to a given hash table
+ *
+ * @ht: pointer to the hash table
+ * @key: key of the hash
+ * @value: value to store
+ * Return: 1 if success, 0 if fails
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int k_index;
+
+	if (ht == NULL)
 		return (0);
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	if (new_node->key == NULL)
-	{
-		free(new_node->key);
-		free(new_node->value);
-		free(new_node);
+
+	if (key == NULL || *key == '\0')
 		return (0);
-	}
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+
+	k_index = key_index((unsigned char *)key, ht->size);
+
+	if (add_n_hash(&(ht->array[k_index]), key, value) == NULL)
+		return (0);
 	return (1);
 }
