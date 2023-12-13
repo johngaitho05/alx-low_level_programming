45cbe7f5e4ac108bb3e77d9cf9d55107af17d3bb
John Gaitho <johngaitho05@gmail.com>
2023-12-13 19:36:04 +0300

    test update

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -2,47 +2,47 @@
 
 /**
  * linear_skip - Searches for a value in a skip list
- * @skip_list: Input skip list
- * @target: Value to search for
+ * @list: Input skip list
+ * @value: Value to search for
  * Return: Node containing the value, or NULL if not found
  */
-skiplist_t *linear_skip(skiplist_t *skip_list, int target)
+skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *current;
 
-	if (skip_list == NULL)
+	if (list == NULL)
 		return (NULL);
 
-	current = skip_list;
+	current = list;
 
-	do
-	{
-		skip_list = current;
+	do {
+		list = current;
 		current = current->express;
 
 		printf("Checking at index ");
 		printf("[%d] = [%d]\n", (int)current->index, current->n);
-	} while (current->express && current->n < target);
+	} while (current->express && current->n < value);
 
 	if (current->express == NULL)
 	{
-		skip_list = current;
+		list = current;
 
 		while (current->next)
 			current = current->next;
 	}
 
 	printf("Value found between indexes ");
-	printf("[%d] and [%d]\n", (int)skip_list->index, (int)current->index);
+	printf("[%d] and [%d]\n", (int)list->index, (int)current->index);
 
-	while (skip_list != current->next)
+	while (list != current->next)
 	{
-		printf("Checking at index [%d] = [%d]\n", (int)skip_list->index, skip_list->n);
+		printf("Checking at index [%d] = [%d]\n",
+			   (int)list->index, list->n);
 
-		if (skip_list->n == target)
-			return (skip_list);
+		if (list->n == value)
+			return (list);
 
-		skip_list = skip_list->next;
+		list = list->next;
 	}
 
 	return (NULL);
