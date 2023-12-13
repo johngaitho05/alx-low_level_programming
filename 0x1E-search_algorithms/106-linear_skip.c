2bdd1ae2af396098c14536c061d614c7f6636710
John Gaitho <johngaitho05@gmail.com>
2023-12-13 19:39:49 +0300

    test

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,47 +1,43 @@
 #include "search_algos.h"
 
 /**
- * linear_skip - Searches for a value in a skip list
- * @list: Input skip list
- * @value: Value to search for
- * Return: Node containing the value, or NULL if not found
+ * linear_skip - searches for a value in a skip list
+ *
+ * @list: input list
+ * @value: value to search in
+ * Return: index of the number
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *current;
+	skiplist_t *now;
 
 	if (list == NULL)
 		return (NULL);
 
-	current = list;
+	now = list;
 
 	do {
-		list = current;
-		current = current->express;
+		list = now;
+		now = now->express;
+		printf("Value checked at index ");
+		printf("[%d] = [%d]\n", (int)now->index, now->n);
+	} while (now->express && now->n < value);
 
-		printf("Checking at index ");
-		printf("[%d] = [%d]\n", (int)current->index, current->n);
-	} while (current->express && current->n < value);
-
-	if (current->express == NULL)
+	if (now->express == NULL)
 	{
-		list = current;
-
-		while (current->next)
-			current = current->next;
+		list = now;
+		while (now->next)
+			now = now->next;
 	}
 
 	printf("Value found between indexes ");
-	printf("[%d] and [%d]\n", (int)list->index, (int)current->index);
+	printf("[%d] and [%d]\n", (int)list->index, (int)now->index);
 
-	while (list != current->next)
+	while (list != now->next)
 	{
-		printf("Checking at index [%d] = [%d]\n",
-			   (int)list->index, list->n);
-
+		printf("Value checked at index [%d] = [%d]\n", (int)list->index, list->n);
 		if (list->n == value)
 			return (list);
-
 		list = list->next;
 	}
 
